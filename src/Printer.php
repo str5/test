c3aa1192c1a530d8d7891dc602c0b3d4d1c61226
Myasnikov Roman <mia358@yandex.ru>
2024-05-23 20:00:56 +0300

    More descriptions

--- src/Printer.php
+++ src/Printer.php
@@ -4,6 +4,9 @@ namespace Str5\Test;
 
 class Printer implements PrinterInterface 
 {
+    /**
+     * Test sescription
+     */
     public function print(string $text, int $count): string
     {
         return str_repeat($text, $count);
